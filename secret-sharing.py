9efd44ab4d3750eb8890f1ef471c9e986dfcc40f
akongstad <kongstad@outlook.com>
2024-05-08 13:53:24 +0200

    Add sample runs and improve comments

--- secret-sharing.py
+++ secret-sharing.py
@@ -1,12 +1,17 @@
-#File is written for Python 3
-#The code uses matplotlib library for plotting the shares as points, so make sure to have it installed.
-#To run the code, use cli and type: "python3 .\secret-sharing.py"
-#The code is tested and are assumed to be working as expected.
-#See comments in the code for more information.
-#The file opens with constants and field operations, then the split_secret function, reconstruct function and test function.
+# File is written for Python 3
+# If you want to visualize the shares as a polynomial make sure to 1. Install matplotlib and 2. Uncomment the import
+#       statement, 3. Uncomment the plot_shares function. 4.Uncomment the plot_shares function call in main.
+
+# To run the code, use cli and type: "python3 .\secret-sharing.py"
+# The code is tested and are assumed to be working as expected.
+# See comments in the code for more information.
+# The file opens with constants and field operations, then the split_secret function, reconstruct function and test function.
+
+# (Note): Function take default param F=F to allow for testing with different fields.
 
 import secrets
-import matplotlib.pyplot as plt
+
+# import matplotlib.pyplot as plt
 
 # CONSTS
 F_HEX = "0x00b44a0bc6303782b729a7f9b44a3611b247ddf1e544f8b1420e2aae976003219175461d2bd76e64ba657d7c9dff6ed7b17980778ec0cbf75fc16e52463e2d784f5f20c1691f17cdc597d7a5141080809a38c635b2a62082e310aa963ca15953894221ad54c6b30aea10f4dd88a66c55ab9c413eae49c0b28e6a3981e0021a7dcb0759af34b095ce3efce78938f2a2bed70939ba47591b88f908db1eadf237a7a7100ac87130b6119d7ae41b35fd27ff6021ac928273c20f0b3a01df1e6a070b8e2e93b5220ad02104000c0c1e82e17fd00f6ac16ef37c3b6153d348e470843a84f25473a51f040a42671cd94ffc989eb27fd42b817f8173bfa95bdfa17a2ae22fd5c89dab2822bcc973b5b90f8fadc9b074cca8f9365b1e8994ff0bda48b1f7498cce02d4e794915f8a4208de3eaf9fbff5"
@@ -73,16 +78,19 @@ def sample_coefficients(k, F=F) -> []:
         yield secrets.randbelow(F)  # Q: Can these be < 0?
 
 
-def split_secret(S, N, k, F=F) -> []:
+def split_secret(S: str, N, k, F=F) -> []:
     """
-    Split the secret S into N shares with the polynomial definition from the lecture
+    Split the secret S into N shares
+    :param S: The secret to split. (bit string)
     :param N: # of shares to split into.
-    :param k: # shares for reconstruction. deg(f) = k - 1.
-    :return: List of N shares with k needed for reconstruction. Share: (i, f(i)).
+    :param k: # shares for reconstruction. I.e. deg(f) = k - 1.
+    :param F: The field to operate in.
+    :return: List of N shares with k needed for reconstruction. Def Share:= (i, f(i)).
     """
-    as_ = [S] + list(sample_coefficients(k - 1, F))  # Random coefficients.
+    s = int(S, 2)
+    as_ = [s] + list(sample_coefficients(k - 1, F))  # Random coefficients.
 
-    def f_(x):  # Polynomial definition of the form:  f(x) = S + a_1x + a_2x^2 + ... + a_k x^k
+    def f_(x):  # Polynomial definition of the form:  f(x) = S + a_1x + a_2x^2 + ... + a_k-1 * x^k-1
         res = 0
         for i, a in enumerate(as_):
             res = F_add(res, F_mul(a, pow(x, i, F), F), F)
@@ -94,11 +102,12 @@ def split_secret(S, N, k, F=F) -> []:
 
 def reconstruct(shares: [], degree, x, F=F) -> int:
     """
-    Reconstruct the polynomial from.
-    :param shares: List of shares. **Shares**: (i, f(i)). **Invariance**: 1. Len(shares) >= degree + 1, 2. i is unique, 3. f(i) \in F.
+    Evaluate the polynomial of degree=degree interpolated from shares at x.
+    :param shares: List of shares. **Shares**: (i, f(i)). **Invariance**: 1. Len(shares) >= k, 2. i is unique, 3. f(i) \in F.
     :param degree: The degree of the polynomial.
     :param x: The x value to evaluate the polynomial at.
-    :return: F(0) - The secret.
+    :param F: The field to operate in.
+    :return: f(x) - value of polynomial at x.
     """
     k = degree + 1
 
@@ -108,30 +117,34 @@ def reconstruct(shares: [], degree, x, F=F) -> int:
     secret = 0
 
     # Lagrange interpolation
-    for i in range(k): 
+    for i in range(k):
         x_i, f_i = shares[i]
         y_i = 1
-        for j in range(k):  
+        for j in range(k):
             if i == j:
                 continue
             x_j, _ = shares[j]
             y_i = F_mul(F_div(F_sub(x, x_j, F), F_sub(x_i, x_j, F), F), y_i, F)
 
         y_i = F_mul(y_i, f_i, F)
-        secret = F_add(secret, y_i, F) 
+        secret = F_add(secret, y_i, F)
 
-    return secret 
+    return secret
 
 
 def test():
+    print("Running tests...")
     # Assertions
     # Field operations
     assert F_add(1, 2) == 3
     assert F_sub(1, 2) == F - 1
     assert F_mul(2, 3) == 6
+    assert F_div(6, 3) == 2
 
     # Split secret
-    shares = split_secret(42, 5, 3)
+    S = 42
+    S_bin = bin(S)[2:]  # [2:] to remove the '0b' prefix. 101010
+    shares = split_secret(S_bin, 5, 3)
     assert len(shares) == 5  # split_secret return N shares.
     assert all([1 <= i <= 5 for i, _ in shares])  # Shares are deterministic and in range
     assert all([a <= F for a in sample_coefficients(5)])  # Random coefficients are in range.
@@ -139,24 +152,40 @@ def test():
     # Reconstruct secret
     assert reconstruct(shares[:3], 2, 0) == 42  # Reconstruct the secret from the shares.
     assert reconstruct(SHARES, 2, 1) == SHARE_1  # Reconstruct the secret from the shares.
-    
-    #Check we can reconstruct SHARE_4 using constructed SHARE_0 with SHARE_1 and SHARE_2
+
+    # Check we can reconstruct SHARE_4 using constructed SHARE_0 with SHARE_1 and SHARE_2
     assert reconstruct([(0, reconstruct(SHARES, 2, 0)), (1, SHARE_1), (2, SHARE_2)], 2, 4) == SHARE_4
-    
+
+    print("All tests passed.\n")
+
+
+# def plot_shares(shares, S):
+#     plt.scatter([0] + [i for i, _ in shares], [S] + [f for _, f in shares])
+#     plt.xlabel("x")
+#     plt.ylabel("f(x)")
+#     plt.show()
+
 
 if __name__ == '__main__':
     test()
-
+    # Example 1
+    print("Running Sample Run 1. N=5, k=3, F=101, S=42...")
     S = 42
-    shares = split_secret(S, 5, 3, 101)
-    assert reconstruct(shares[:5], 2, 0, 101) == S
-    print(shares)
+    S_bin = bin(S)[2:]  # [2:] to remove the '0b' prefix. 101010
+    print(f"The secret is: {S} == 101010")
 
+    shares = split_secret(S_bin, 5, 3, 101)
+    print("The secret is split into 5 shares with 3 needed for reconstruction.")
+    print("Shares: ", shares)
+
+    print(f"Reconstructing the secret using the first 3 shares: {shares[:3]}")
+    reconstructed_secret = reconstruct(shares[:3], 2, 0, 101)
+    assert reconstructed_secret == S
+    print(f"The reconstructed secret is: {reconstructed_secret} == {S}\n")
     # Plot the shares as points.
-    plt.scatter([0] + [i for i, _ in shares], [S] + [f for _, f in shares])
-    plt.xlabel("x")
-    plt.ylabel("f(x)")
-    plt.show()
+    # plot_shares(shares, S)
 
+    # Examples 2
+    print(f"Running Sample Run 2. N=5, k=3, F={F_HEX}, S=?...")
     share_0 = reconstruct(SHARES, 2, 0)
-    print("The reconstructed secret is: ", hex(share_0)[2:])
+    print("The reconstructed secret/share_0 is: ", hex(share_0)[2:])
